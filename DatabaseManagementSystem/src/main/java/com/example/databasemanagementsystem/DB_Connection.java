2f9a01be3d47228518e46703c158aa252c97b849
ElioAl <elio_a@icloud.com>
2024-10-07 22:44:12 -0300

    Connected database and retrieved a user

--- DatabaseManagementSystem/src/main/java/com/example/databasemanagementsystem/DB_Connection.java
+++ DatabaseManagementSystem/src/main/java/com/example/databasemanagementsystem/DB_Connection.java
@@ -1,23 +1,31 @@
 package com.example.databasemanagementsystem;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.io.FileInputStream;
 import java.sql.*;
 import java.util.Properties;
 
 public class DB_Connection {
-    public static Connection connect(){
+
+    public Connection connect(){
         Connection dbConnection = null;
-        Properties dbConnectionProps = new Properties();
+        //Properties dbConnectionProps = new Properties();
 
         try{
-            FileInputStream inputStream = new FileInputStream("/src/main/resources/application.properties");
-            dbConnectionProps.load(inputStream);
+//            FileInputStream inputStream = new FileInputStream("main/resources/db.properties");
+//            dbConnectionProps.load(inputStream);
+//
+//            inputStream.close();
+//
+//            String url = dbConnectionProps.getProperty("url");
+//            String username = dbConnectionProps.getProperty("username");
+//            String password = dbConnectionProps.getProperty("password");
 
-            inputStream.close();
-
-            String url = dbConnectionProps.getProperty("url");
-            String username = dbConnectionProps.getProperty("username");
-            String password = dbConnectionProps.getProperty("password");
+            String url = "jdbc:mysql://ev-registration-system.mysql.database.azure.com:3306/evregistrationsystem";
+            String username="admin101";
+            String password="EvRegistrationSystem!";
 
             dbConnection = DriverManager.getConnection(url, username, password);
 
