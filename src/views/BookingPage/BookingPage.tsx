a2068a49de6c96010bfdfe01589c8a98de5290d4
ElioAl <elio_a@icloud.com>
2024-10-23 23:00:38 -0300

    Fixed Modal View

--- src/views/BookingPage/BookingPage.tsx
+++ src/views/BookingPage/BookingPage.tsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
 import Calendar from '../../components/Bookings/Calendar';
+import ReservationModal from '../../components/Bookings/ReservationModal';
 
 const BookingPage = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Welcome to the EV Registration System</h1>
-            <Calendar />
+            <Calendar/>
+            {/* Buttons */}
+            <div style={{ display: 'flex', justifyContent: 'space-between', width: '50%', margin: '20px auto' }}>
+                <button className="button" onClick={openModal}>Make a Reservation</button>
+                <button className="button" onClick={() => console.log('Delete clicked')}>Cancel Reservation</button>
+                <button className="button" onClick={() => console.log('Update clicked')}>Modify Reservation</button>
+            </div>
+            {isModalOpen && (
+                <ReservationModal onClose={closeModal} isOpen />
+            )}
         </div>
     );
 };
