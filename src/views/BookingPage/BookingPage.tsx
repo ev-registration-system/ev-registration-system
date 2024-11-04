273b311405c9b9a692f7afc5ef2010d204bb991e
Christopher Pelleter <cpellet2@unb.ca>
2024-11-04 18:22:24 -0400

    added fetchbookings for edit boking

--- src/views/BookingPage/BookingPage.tsx
+++ src/views/BookingPage/BookingPage.tsx
@@ -1,10 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Calendar from '../../components/Bookings/Calendar';
 import ReservationModal from '../../components/Bookings/ReservationModal';
+import { collection, addDoc, getDocs, Timestamp } from "firebase/firestore";
+import { db } from "../../../firebase";
+
+
 
 const BookingPage = () => {
-
+    const [bookings, setBookings] = useState([]); // State to hold fetched bookings
     const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        const fetchBookings = async () => {
+            try {
+                const bookingsCollection = collection(db, "bookings");
+                const bookingsSnapshot = await getDocs(bookingsCollection);
+                const bookings = bookingsSnapshot.docs.map((doc, index) => ({
+                    id: index, // Using index as an identifier
+                    docId: doc.id, // Firestore document ID
+                    ...doc.data(), // Document data
+                }));
+                console.log("Bookings:", bookings); // Logs all bookings to console
+            } catch (error) {
+                console.error("Error fetching bookings:", error);
+            }
+        };
+
+        fetchBookings();
+    }, []);
+
+    
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -14,6 +39,12 @@ const BookingPage = () => {
         setIsModalOpen(false);
     };
 
+    const editBooking = () => {
+        const ref = collection(db, "bookings");
+
+
+    }
+
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Welcome to the EV Registration System</h1>
@@ -22,7 +53,7 @@ const BookingPage = () => {
             <div style={{ display: 'flex', justifyContent: 'space-between', width: '50%', margin: '20px auto' }}>
                 <button className="button" onClick={openModal}>Make a Reservation</button>
                 <button className="button" onClick={() => console.log('Delete clicked')}>Cancel Reservation</button>
-                <button className="button" onClick={() => console.log('Update clicked')}>Modify Reservation</button>
+                <button className="button" onClick={editBooking}>Modify Reservation</button>
             </div>
             {isModalOpen && (
                 <ReservationModal onClose={closeModal} isOpen />
