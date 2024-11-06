5e869454552c17cbfd5c52488fac9f51505d2456
Ryan Pitre <rpitre2@unb.ca>
2024-11-06 17:39:10 -0400

    EVRS-13: Adding login page and add some routing

--- src/views/BookingPage/BookingPage.tsx
+++ src/views/BookingPage/BookingPage.tsx
@@ -1,9 +1,27 @@
 import Calendar from '../../components/Bookings/Calendar';
+import Button from '../../components/Bookings/Button';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../../firebase';
+import { signOut } from 'firebase/auth';
 
 const BookingPage = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth); // Logs the user out
+            navigate('/login'); // Redirect to login page
+        } catch (error) {
+            console.error('Failed to log out:', error);
+        }
+    };
+
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Welcome to the EV Registration System</h1>
+            <Button onClick={handleLogout}>
+                Logout
+            </Button>
             <Calendar />
         </div>
     );
