ee4de1b3af23df1ca078d0a9f3679382488716d3
ElioAl <elio_a@icloud.com>
2024-11-16 15:44:18 -0400

    Revert "Merge branch 'main' into EVRS-25"
    This reverts commit b113307f1960b81b18fcd074823162a0394b2577, reversing
    changes made to dced1c2280f0d3f29b372a017e61c928f17f9a4b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import BookingPage from './views/BookingPage/BookingPage';
-import LoginPage from './views/LoginPage/LoginPage';
-import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
-import AppLayout from './components/AppLayout/AppLayout';
-import { AuthProvider } from './state/AuthProvider/AuthProvider';
+// import React from 'react';
+// import { db } from './firebase'
+// import { collection, getDocs } from 'firebase/firestore'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import BookingPage from './views/BookingPage/BookingPage'
 
-const App: React.FC = () => {
+function App() {
+
     return (
         <Router>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    
-                    {/* Protected routes wrapper */}
-                    <Route element={<ProtectedRoutes />}>
-                        {/* App layout wrapper */}
-                        <Route path="/" element={<AppLayout />}>
-                            <Route index element={<BookingPage />} />
-                        </Route>
-                    </Route>
-                    
-                    {/* Catch-all route */}
-                    <Route path="*" element={<Navigate to="/" replace />} />
-                </Routes>
-            </AuthProvider>
+            <Routes>
+                <Route path="/" element={<BookingPage />} />
+            </Routes>
         </Router>
     );
-};
+}
 
 export default App;
+
+
+
+// <CreateBooking />
--- src/views/BookingPage/BookingPage.tsx
+++ src/views/BookingPage/BookingPage.tsx
@@ -1,21 +1,6 @@
 import { useState } from 'react';
 import Calendar from '../../components/Bookings/Calendar';
 import ReservationModal from '../../components/Bookings/ReservationModal';
-import Button from '../../components/Bookings/Button';
-import { useNavigate } from 'react-router-dom';
-import { auth } from '../../../firebase';
-import { signOut } from 'firebase/auth';
-
-const BookingPage = () => {
-    const navigate = useNavigate();
-
-    const handleLogout = async () => {
-        try {
-            await signOut(auth); // Logs the user out
-            navigate('/login'); // Redirect to login page
-        } catch (error) {
-            console.error('Failed to log out:', error);
-}
 
 const BookingPage = () => {
 
@@ -32,9 +17,6 @@ const BookingPage = () => {
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Welcome to the EV Registration System</h1>
-            <Button onClick={handleLogout}>
-                Logout
-            </Button>
             <Calendar/>
             {/* Buttons */}
             <div style={{ display: 'flex', justifyContent: 'space-between', width: '50%', margin: '20px auto' }}>
