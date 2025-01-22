1698203c30f385c480fe9c73e616ca0573931419
smossman <114663957+smossman@users.noreply.github.com>
2025-01-22 11:09:31 -0400

    Edited Login Page and Restructured Pages

--- src/views/BookingPage/BookingPage.tsx
+++ src/views/BookingPage/BookingPage.tsx
@@ -5,7 +5,7 @@ import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../../../firebase'
 import { Booking } from '../../types/types'
 import { tokens } from '../../Theme'
-import { Button, useTheme } from '@mui/material'
+import { Box, Button, useTheme } from '@mui/material'
 
 const ref = collection(db, 'bookings')
 
@@ -13,10 +13,6 @@ const BookingPage = () => {
 	const [bookings, setBookings] = useState<Booking[]>([])
 	const [loading, setLoading] = useState(true)
 	const [isModalOpen, setIsModalOpen] = useState(false)
-	const [isGraphVisible, setIsGraphVisible] = useState(false)
-	const [plotImage, setPlotImage] = useState<string | null>(null) // State to hold the plot image for emissions
-	const [currentEmission, setCurrentEmission] = useState<number | null>(null) // State for current emission
-	const [currentHour, setCurrentHour] = useState<number | null>(null) // State for current hour
 	const theme = useTheme()
 	const colors = tokens(theme.palette.mode)
 
@@ -49,96 +45,18 @@ const BookingPage = () => {
 		setIsModalOpen(false)
 	}
 
-	// Toggle visibility of emissions graph after clicking button
-	const toggleGraph = async () => {
-		if (isGraphVisible) {
-			setIsGraphVisible(false) // If graph is visible, hide it
-		} else {
-			try {
-				const response = await fetch('http://127.0.0.1:5000/plot') // Network request to the backend server where Flask is running
-				if (!response.ok) {
-					throw new Error('Failed to fetch plot image') // Throw error if image can't be fetched
-				}
-				// 'blob' (holding img contents in binary) is waiting for server response
-				const blob = await response.blob()
-				// Creates temporary URL for the Blob object
-				const imageUrl = URL.createObjectURL(blob)
-				// State to hold plot imgage ('plotImage') is updated
-				setPlotImage(imageUrl)
-				// Display graph
-				setIsGraphVisible(true)
-			} catch (error) {
-				console.error('Error fetching plot:', error)
-			}
-		}
-	}
-
-	// Fetch the current emission data for the current hour
-	const fetchCurrentEmission = async () => {
-		try {
-			const response = await fetch('http://127.0.0.1:5000/get-number') // Network request to the backend server where Flask is running
-			// JSONs data is stored in 'data' once server responds
-			const data = await response.json()
-
-			if (data.current_emission !== undefined) {
-				setCurrentEmission(data.current_emission) // Set current emission in state
-				setCurrentHour(data.hour) // Set current hour in state
-			} else {
-				console.error('Error fetching current emission:', data.error) // Throw error if unable to fetch JSON from server
-			}
-		} catch (error) {
-			console.error('Error fetching current emission:', error)
-		}
-	}
-	// fetchCurrentEmission() runs when the componnet mounts (when page is loaded)
-	useEffect(() => {
-		fetchCurrentEmission()
-	}, []) // Empty brackets tells React to run the useEffect hook only once
-
-	/*
-    If we get data that is real-time (updates every minute) then we can use:
-    useEffect(() => {
-    const interval = setInterval(() => {fetchCurrentEmission()}, 60000); // 60000 ms = 1 minute, update every minute
-
-    // Clean up the interval when the component unmounts
-    return () => clearInterval(interval)}, []);
-
-    */
-
 	return (
+		<Box
+		display="flex"
+		justifyContent="center"
+		alignItems="center"
+		sx={{
+			backgroundColor: colors.primary[400],
+			padding: 2,
+			borderRadius: 2,
+		}}
+	>
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            {currentEmission !== null && currentHour !== null && (
-                <div style={{ marginTop: '20px' }}>
-                    <h3>Current Emission for Hour {currentHour}: {currentEmission} kg CO2 per kWh</h3>
-                </div>
-            )}
-
-
-
-            <div style={{ display: 'flex', justifyContent: 'space-between', width: '50%', margin: '20px auto' }}>
-                <Button
-                    variant="contained"
-                    sx={{
-                        color: colors.grey[100],
-                        backgroundColor: colors.primary[400],
-                        fontWeight: "bold",
-                        '&:hover': {
-                            backgroundColor: colors.accent[400]
-                        },
-                    }}
-                    onClick={toggleGraph}
-                >
-                    {isGraphVisible ? 'Hide Emissions Graph' : 'Show Emissions Graph'}
-                </Button>
-            </div>
-			
-			{/* Buttons */}
-
-            {isGraphVisible && plotImage && (
-                <div style={{ marginTop: '20px' }}>
-                    <img src={plotImage} alt="Hourly Emissions Plot" style={{ width: '80%', maxWidth: '800px' }} />
-                </div>
-            )}
             <div
             style={{
                 height: '100%',
@@ -146,12 +64,12 @@ const BookingPage = () => {
                 width: '100%',
             }}
             >
-            {loading ? (
-                <p>Loading bookings...</p>
-            ) : (
+            	{loading ? (
+                	<p>Loading bookings...</p>
+            	) : (
                 
                 <Calendar bookings={bookings} getBookings={getBookings}/>
-            ) }
+            	) }
             </div>
 
             {/* Buttons */}
@@ -179,6 +97,7 @@ const BookingPage = () => {
                 />
             )}
         </div>
+		</Box>
     );
 };
 
