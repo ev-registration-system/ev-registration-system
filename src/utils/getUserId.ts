6e22b21f0f15867b43b5af3c35d845d63dd8c71b
smossman <114663957+smossman@users.noreply.github.com>
2025-02-19 16:07:46 -0400

    Modified Calendar Tests to reflect changes

--- src/utils/getUserId.ts
+++ src/utils/getUserId.ts
@@ -1,6 +1,10 @@
 import { getAuth } from "firebase/auth";
 
 export const getUserId = (): string | null => {
+    //This sets up user id for unit tests
+    if (process.env.NODE_ENV === "test") {
+        return "mock-user-id"; //Mock user ID for tests
+    }
     const auth = getAuth();
     const currentUser = auth.currentUser;
 
